728c7bfb90d6cc984703428783468f210de65642
Kainth Sourav <35909895+kainthsourav@users.noreply.github.com>
2019-09-29 11:01:11 +0530

    Add project files.

---
+++ MOD_API/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using MOD_BAL;
+using MOD_DATA;
+
+namespace MOD_API.Controllers
+{
+    public class UserController : ApiController
+    {
+        MOD_BAL.UserLogic userLogic = new MOD_BAL.UserLogic();
+
+        [Route("api/getuser")]
+        [HttpGet]
+        public IHttpActionResult GetAllUser()
+        {
+            var result = userLogic.GetAllUsers();
+            return Ok(result);
+        }
+    }
+}
---
+++ MOD_BAL/UserLogic.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using MOD_DATA;
+
+namespace MOD_BAL
+{
+   public class UserLogic
+    {
+        public MOD_DBEntities data = new MOD_DBEntities();
+        
+        public IList<UserDtl> GetAllUsers()
+        {
+            return data.UserDtls.ToList();
+        }
+
+        public UserDtl GetUserById(int id)
+        {
+            return data.UserDtls.Find(id);
+        }
+
+        public void Register(UserDtl userDtl)
+        {
+            //Adding Trainer
+            if(userDtl.role==2)
+            {
+                var newTrainer = new UserDtl()
+                {
+                    userName = userDtl.userName,
+                    password = userDtl.password,
+                    email = userDtl.email,
+                    firstName = userDtl.firstName,
+                    lastName = userDtl.lastName,
+                    contactNumber = userDtl.contactNumber,
+                    linkdinUrl = userDtl.linkdinUrl,
+                    yearOfExperience = userDtl.yearOfExperience,
+                    confirmedSignUp=userDtl.confirmedSignUp,
+                    active=userDtl.active
+                };
+                data.UserDtls.Add(newTrainer);
+               
+            }
+
+            //Adding User
+            if(userDtl.role==3)
+            {
+                var newUser = new UserDtl()
+                {
+                    userName = userDtl.userName,
+                    password = userDtl.password,
+                    firstName = userDtl.firstName,
+                    lastName = userDtl.lastName,
+                    contactNumber = userDtl.contactNumber,
+                    yearOfExperience = userDtl.yearOfExperience,
+                    linkdinUrl = userDtl.linkdinUrl,
+                    confirmedSignUp = userDtl.confirmedSignUp,
+                    active = userDtl.active
+                };
+                data.UserDtls.Add(newUser);
+            }
+
+            //save data to database
+            data.SaveChanges();
+        }
+
+        public void DeleteUser(int id)
+        {
+            UserDtl user = data.UserDtls.Find(id);
+            data.UserDtls.Remove(user);
+            data.SaveChanges();
+        }
+
+        public void UpdateUser(UserDtl userDtl)
+        {
+            data.Entry(userDtl).State = EntityState.Modified;
+            data.Configuration.ValidateOnSaveEnabled = false;
+            data.SaveChanges();
+            data.Configuration.ValidateOnSaveEnabled = true;
+        }
+    }
+}
